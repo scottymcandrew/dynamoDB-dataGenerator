375375315eeae8bb3e4c03f06a9d97b19e626f12
Scott McAndrew <scottymcandrew@outlook.com>
2019-05-07 08:25:20 +0100

    Changed JSON & DynamoDB format

--- generate-octet.sh
+++ generate-octet.sh
@@ -4,16 +4,16 @@
 # Arg 1 specifies the subnet label e.g. mgmt
 # Arg 2 specifies the destination file
 
-echo \[ >> $2
+echo \[ >> $1
 
 for i in {5..250..1}
     do
         if [[ $i -lt 250 ]]; then
-            echo \{ \"SubnetName\": \"$1-$i\", \"SubnetType\": \"Management\", \"SubnetNumber\": \"192.168.$i.0\/24\", \"InUse\": false \}, >> $2
+            echo \{ \"SubnetName\": \"Octet-$i\", \"SubnetNumber\": \"$i\", \"InUse\": false \}, >> $1
         fi
         if [[ $i -eq 250 ]]; then
-            echo \{ \"SubnetName\": \"$1-$i\", \"SubnetType\": \"Management\", \"SubnetNumber\": \"192.168.$i.0\/24\", \"InUse\": false \} >> $2
+            echo \{ \"SubnetName\": \"Octet-$i\", \"SubnetNumber\": \"$i\", \"InUse\": false \} >> $1
         fi 
     done
 
-echo \] >> $2
+echo \] >> $1
